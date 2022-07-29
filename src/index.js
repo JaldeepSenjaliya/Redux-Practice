1214627c3c2ad22467b66097e5bbe74e0e0c00f2
Jaldeep Patel <61433142+JaldeepSenjaliya@users.noreply.github.com>
2022-07-29 18:18:46 -0400

    Updated Index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const wrap = (type, str)=> `<${type}>${str}</${type}>`;
 const lowerCase = str => str.toLowerCase();
 
 const transform = pipe(trim, lowerCase, wrap("div", "hello "));
+const transform = pipe(trim, lowerCase, wrap("div", "hello "));
+const transform = pipe(trim, lowerCase, wrap("div", "hello "));
+
 console.log(wrap("span", "Hello World"));
 
 console.log(transform(input));
